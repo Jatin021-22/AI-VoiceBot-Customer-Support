2fdfc6f04ab8101ed7858c1da0d1180d70b4e39f
Jatin Prajapati <jatinprajapati0658@gmail.com>
2026-03-13 13:40:05 +0530

    fix ffmpeg path to work on any system

--- app/core/asr.py
+++ app/core/asr.py
@@ -4,9 +4,8 @@ import tempfile
 import subprocess
 import shutil
 import numpy as np
-
-FFMPEG_PATH = r"C:\Users\tankr\AppData\Local\Microsoft\WinGet\Packages\Gyan.FFmpeg_Microsoft.Winget.Source_8wekyb3d8bbwe\ffmpeg-8.0.1-full_build\bin\ffmpeg.exe"
-
+import shutil
+FFMPEG_PATH = shutil.which("ffmpeg") or "ffmpeg"
 
 class WhisperASR:
     _model = None
